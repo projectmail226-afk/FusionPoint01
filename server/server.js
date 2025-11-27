336c1a7cb16f5f440aaf116bb2f9b8a227d25750
projectmail226-afk <projectmail226@gmail.com>
2025-11-28 00:33:43 +0600

    update

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import connectDB from './configs/db.js';
 import {inngest, functions} from './inngest/index.js'
 import {serve} from 'inngest/express'
+import serverless from 'serverless-http';
 
 const app =express();
 
